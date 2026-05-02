2c5d933df81d5b433c06482cb35fe1c86d919ec6
NelsonCofre <nelsoncofrew@gmail.com>
2026-05-02 11:22:39 -0400

    Refactorizacion de ms

---
+++ backend/necessity-service/src/main/java/com/donaton/necessity/NecessityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.donaton.necessity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NecessityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NecessityServiceApplication.class, args);
+	}
+
+}
---
+++ backend/necessity-service/src/test/java/com/donaton/necessity/NecessityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donaton.necessity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NecessityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
