896515ef0e3ecf21a8c1f3050d698bb873be38b9
NelsonCofre <nelsoncofrew@gmail.com>
2026-05-02 10:21:56 -0400

    Primer commit

---
+++ backend/necessity-service/src/test/java/com/dontaton/necessity/NecessityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dontaton.necessity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NecessityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
