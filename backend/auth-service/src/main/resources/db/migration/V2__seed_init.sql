0d319e087f70ebbe28b7291d18a360c104cb9c2d
mariocofre <mariocofre2023@gmail.com>
2026-05-12 19:10:33 -0400

    Volver

--- backend/auth-service/src/main/resources/db/migration/V2__seed_init.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO roles (nombre, descripcion) VALUES ('ROLE_ADMIN', 'Administrador total');
-INSERT INTO roles (nombre, descripcion) VALUES ('ROLE_DONANTE', 'Persona que realiza donaciones');
